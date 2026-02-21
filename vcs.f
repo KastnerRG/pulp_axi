70f2a315d4aa27c152cf69644ec51497d8af8a65
abarajithan11 <abarajithan07@gmail.com>
2026-02-21 15:09:16 -0800

    Add support for vcs

---
+++ vcs.f
@@ -0,0 +1,9 @@
+src/axi_pkg.sv
+src/axi_intf.sv
+-f vcs_axi_backend.f
+../common_verification/src/rand_id_queue.sv
+../common_verification/src/rand_verif_pkg.sv
+../common_verification/src/clk_rst_gen.sv
+src/axi_test.sv
+src/axi_sim_mem.sv
+../tech_cells_generic/src/rtl/tc_sram.sv
